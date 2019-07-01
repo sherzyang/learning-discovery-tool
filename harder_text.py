578c4925a05010977954857dd8069a217c060fc4
Sherry <sherry.yang33@gmail.com>
2019-07-01 14:59:53 -0700

    Add mongodb file

---
+++ harder_text.py
@@ -0,0 +1,35 @@
+import textstat
+import operator
+import wikipedia
+import wikipediaapi
+
+def give_harder_level(text):
+    """
+    Takes in text and returns an easier level read
+    """
+    ##find all the articles that are easier than this article 
+    all_harder_text = []
+    input_score = textstat.flesch_kincaid_grade(text)
+    for i in range(len(df_corpus2['score'])):
+        if df_corpus2['score'][i] > input_score:
+            all_harder_text.append(df_corpus2['content'][i])
+    
+    vec = TfidfVectorizer(stop_words='english', max_features=2000)
+    vec = vec.fit(all_harder_text)
+    corpus2_vectors = vec.transform(all_harder_text).toarray()
+    user_doc = text
+    user_doc_vector = vec.transform([user_doc]).toarray()    
+    distances = cdist(user_doc_vector,
+                  corpus2_vectors,
+                  metric='cosine')[0]
+    ranking = np.argsort(distances)
+    top = ranking[0]
+    best_match = df_corpus2['content'][top]
+    
+#     best_matches = []
+#     for i in top:
+#         temp = df_corpus2['content'][i]
+#         best_match.append(temp)
+        
+    print(distances[top])
+    return (best_match)
