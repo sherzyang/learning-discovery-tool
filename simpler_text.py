30a2d2932ab510382981c2359f23eec2d4b8aa7b
Sherry <sherry.yang33@gmail.com>
2019-07-01 16:25:08 -0700

    Add website

--- simpler_text.py
+++ simpler_text.py
@@ -3,20 +3,20 @@ import operator
 import wikipedia
 import wikipediaapi
 
-def give_harder_level(text):
+def give_simpler_level(text):
     """
     Takes in text and returns an easier level read
     """
     ##find all the articles that are easier than this article 
-    all_harder_text = []
+    all_simpler_text = []
     input_score = textstat.flesch_kincaid_grade(text)
     for i in range(len(df_corpus2['score'])):
-        if df_corpus2['score'][i] > input_score:
-            all_harder_text.append(df_corpus2['content'][i])
+        if df_corpus2['score'][i] < input_score:
+            all_simpler_text.append(df_corpus2['content'][i])
     
     vec = TfidfVectorizer(stop_words='english', max_features=2000)
-    vec = vec.fit(all_harder_text)
-    corpus2_vectors = vec.transform(all_harder_text).toarray()
+    vec = vec.fit(all_simpler_text)
+    corpus2_vectors = vec.transform(all_simpler_text).toarray()
     user_doc = text
     user_doc_vector = vec.transform([user_doc]).toarray()    
     distances = cdist(user_doc_vector,
