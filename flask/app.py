a51acda2be6bd17080f8a8d1d5c9dbf1d09329bd
Sherry <sherry.yang33@gmail.com>
2019-07-03 22:05:18 -0700

    Add working flask app version 1

--- flask/app.py
+++ flask/app.py
@@ -6,24 +6,15 @@ from sklearn.pipeline import Pipeline
 import pickle
 from flask import Flask, request, render_template, jsonify
 
-
-# with open('spam_model.pkl', 'rb') as f:
-#     model = pickle.load(f)
+import os
+import numpy as np
+import textstat
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cosine, cdist
 
 with open(r"data/df_corpus2.pkl", "rb") as input_file:
     df_corpus2 = pickle.load(input_file)
 
-with open (r"data/lda_model_1.pkl", "rb") as input_file:
-    lda_model_1 = pickle.load(input_file)
-    
-with open(r"data/df_best_category.pkl", "rb") as input_file:
-    df_best_category = pickle.load(input_file)
-    
-with open (r"data/dictionary.pkl", "rb") as input_file: 
-    dictionary = pickle.load(input_file)
-    
-   
-    
 app = Flask(__name__, static_url_path="")
 
 @app.route('/')
@@ -31,11 +22,54 @@ def index():
     """Return the main page."""
     return render_template('index.html')
 
+def load_vectorizer(pickle_file='data/vectorizer.pkl'):
+    """Loads the trained TF/IDF vectorizer."""
+    with open(pickle_file, 'rb') as f:
+        return pickle.load(f)
+    
+def load_corpus_vectors(pickle_file='data/corpus_vectors.pkl'):
+    """Loads the corpus vectors."""
+    with open(pickle_file, 'rb') as f:
+        return pickle.load(f)
+    
+def get_vocab_arr(vec):
+    n_features = len(vec.vocabulary_)
+    vocab_arr = np.empty(n_features, dtype=object)
+    for word, idx in vec.vocabulary_.items():
+        vocab_arr[idx] = word
+    return vocab_arr
+
+def get_top_k_vector(vector, feature_ranking, k=20):
+    """Return the top k vector according to feature_ranking."""
+    return vector[:, feature_ranking[:k]]
+
+def vectorize_text(text):
+    vec = load_vectorizer()
+    corpus_vectors = load_corpus_vectors().toarray()
+    sample_vector = vec.transform([text]).toarray()
+    feature_ranking = np.argsort(sample_vector[0])[::-1]
+    vocab_arr = get_vocab_arr(vec)
+    
+    distances = cdist(
+    get_top_k_vector(sample_vector, feature_ranking),
+    get_top_k_vector(corpus_vectors, feature_ranking),
+    )
+    
+    nearest_article_idxs = np.argsort(distances)
+    nearest_articles = df_corpus2.loc[nearest_article_idxs[0], :]
+    top_10 = nearest_articles[:10]
+    top_df = top_10.sort_values(['score'])
+    article_num = str(nearest_articles[:1].index)
+    article_num_str = article_num.strip("Int64Index([], dtype='int64')")
+    article_num_final = int(article_num_str)
+     
+    return nearest_articles.loc[article_num_final, 'content']
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    """Return a random prediction."""
+    """Return a new article."""
     data = request.json
-    prediction = model.predict_proba([data['user_input']])
-    return jsonify({'probability': prediction[0][1]})
+    data = str(data)
+    result = vectorize_text(data)
+    return jsonify(result)
 
