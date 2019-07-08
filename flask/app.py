6a46e1a6f43103c4ced73cb03742b0dc437adb5a
Sherry <sherry.yang33@gmail.com>
2019-07-08 12:04:17 -0700

    make S3 bucket files accessible to app.py

--- flask/app.py
+++ flask/app.py
@@ -6,34 +6,49 @@ from sklearn.pipeline import Pipeline
 import pickle
 from flask import Flask, request, render_template, jsonify
 
-
 import os
 import numpy as np
 import textstat
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.spatial.distance import cosine, cdist
 
-with open(r"data/new_corpus.pkl", "rb") as input_file:
+app = Flask(__name__, static_url_path="")
+    
+# with open(r"data/new_corpus.pkl", "rb") as input_file:
+#     new_corpus = pickle.load(input_file)
+
+def download_and_open(url, mode='rb', folder='data'):
+    """Downloads the specified file if it isn't already downloaded, then opens it."""
+    filename = os.path.basename(url)
+    pathname = os.path.join(folder, filename)
+    if not os.path.exists(pathname):
+        response = requests.get(url)
+        with open(pathname, 'wb') as f:
+            f.write(response.content)
+    return open(pathname, mode)
+
+with download_and_open(r"https://text-ascent.s3-us-west-2.amazonaws.com/new_corpus.pkl", "rb") as input_file:
     new_corpus = pickle.load(input_file)
     
-with open (r"data/dictionary.pkl", "rb") as input_file: 
-    dictionary = pickle.load(input_file)
-    
-app = Flask(__name__, static_url_path="")
+# with open (r"data/dictionary.pkl", "rb") as input_file: 
+#     dictionary = pickle.load(input_file)
+
+with download_and_open(r"https://text-ascent.s3-us-west-2.amazonaws.com/dictionary.pkl", "rb") as input_file: 
+     dictionary = pickle.load(input_file)
 
 @app.route('/')
 def index():
     """Return the main page."""
     return render_template('index.html')
 
-def load_vectorizer(pickle_file='data/vectorizer.pkl'):
+def load_vectorizer(pickle_file='https://text-ascent.s3-us-west-2.amazonaws.com/vectorizer.pkl'):
     """Loads the trained TF/IDF vectorizer."""
-    with open(pickle_file, 'rb') as f:
+    with download_and_open(pickle_file, 'rb') as f:
         return pickle.load(f)
     
-def load_corpus_vectors(pickle_file='data/corpus_vectors.pkl'):
+def load_corpus_vectors(pickle_file='https://text-ascent.s3-us-west-2.amazonaws.com/corpus_vectors.pkl'):
     """Loads the corpus vectors."""
-    with open(pickle_file, 'rb') as f:
+    with download_and_open(pickle_file, 'rb') as f:
         return pickle.load(f)
     
 def get_vocab_arr(vec):
@@ -71,14 +86,6 @@ def vectorize_text(text):
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    """Return a new article."""
-    data = request.json
-    data = str(data)
-    result = vectorize_text(data)
-    return jsonify(result)
-
-@app.route('/level', methods=['GET', 'POST'])
-def level():
     """Return a new article."""
     data = request.json
     data = str(data)
